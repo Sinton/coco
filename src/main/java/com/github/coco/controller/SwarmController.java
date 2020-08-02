42788aacf17061e99862a724dc0dbc6c169b286f
Sinton <yansuntong@gmail.com>
2020-08-03 00:08:27 +0800

    add docker swarm init

--- src/main/java/com/github/coco/controller/SwarmController.java
+++ src/main/java/com/github/coco/controller/SwarmController.java
@@ -4,6 +4,7 @@ import com.github.coco.annotation.WebLog;
 import com.github.coco.constant.GlobalConstant;
 import com.github.coco.constant.dict.ErrorCodeEnum;
 import com.github.coco.utils.LoggerHelper;
+import com.spotify.docker.client.messages.swarm.SwarmInit;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,26 @@ import java.util.Map;
 @RestController
 @RequestMapping(value = "/api/swarm")
 public class SwarmController extends BaseController {
+
+    @WebLog
+    @PostMapping(value = "/init")
+    public Map<String, Object> initSwarm(@RequestBody Map<String, Object> params) {
+        try {
+            if (dockerClient.info().swarm() == null) {
+                SwarmInit swarmInit = SwarmInit.builder()
+                                               .advertiseAddr("192.168.3.140")
+                                               .listenAddr("192.168.3.140")
+                                               .build();
+                dockerClient.initSwarm(swarmInit);
+            } else {
+                return apiResponseDTO.returnResult(GlobalConstant.SUCCESS_CODE, "初始化Swarm集群节点成功");
+            }
+            return apiResponseDTO.returnResult(ErrorCodeEnum.EXCEPTION.getCode(), "已Swarm集群化");
+        } catch (Exception e) {
+            LoggerHelper.fmtError(getClass(), e, "初始化Swarm集群节点失败");
+            return apiResponseDTO.returnResult(ErrorCodeEnum.EXCEPTION.getCode(), "初始化Swarm集群节点失败");
+        }
+    }
 
     @WebLog
     @PostMapping(value = "/inspect")
