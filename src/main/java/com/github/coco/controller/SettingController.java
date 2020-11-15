b20159fecb218ddf17ab2fd0219c3cd9303f79d9
Sinton <yansuntong@gmail.com>
2020-11-15 15:14:21 +0800

    init install logic

--- src/main/java/com/github/coco/controller/SettingController.java
+++ src/main/java/com/github/coco/controller/SettingController.java
@@ -1,7 +1,12 @@
 package com.github.coco.controller;
 
+import com.github.coco.annotation.WebLog;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * @author Yan
@@ -9,4 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/api/setting")
 public class SettingController extends BaseController {
+    @WebLog
+    @PostMapping(value = "/install")
+    public Map<String, Object> install(@RequestBody Map<String, Object> params) {
+        return null;
+    }
 }
---
+++ src/main/resources/sql/init.sql
@@ -0,0 +1,17 @@
+create table t_user
+(
+    uid             integer not null constraint t_user_pk primary key autoincrement,
+    username        text    not null,
+    salt            text    not null,
+    password        text    not null,
+    nickname        text,
+    avatar          text    default 'avatar.jpg',
+    status          integer default 0,
+    email           text,
+    telephone       text,
+    lang            text    default 'zh-CN',
+    last_login_ip   text,
+    last_login_time integer,
+    creator_id      integer,
+    create_time     integer
+);
