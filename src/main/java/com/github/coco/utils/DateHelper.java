00521484bf0d07c74bc1e087b9fdc1cf2667b353
Sinton <yansuntong@gmail.com>
2020-11-15 14:56:01 +0800

    update date utils logic

--- src/main/java/com/github/coco/utils/DateHelper.java
+++ src/main/java/com/github/coco/utils/DateHelper.java
@@ -4,4 +4,20 @@ package com.github.coco.utils;
  * @author Yan
  */
 public class DateHelper {
+    /**
+     * 一秒钟
+     */
+    public static final int SECOND = 1000;
+    /**
+     * 一分钟
+     */
+    public static final int MINUTE = 60 * SECOND;
+    /**
+     * 一小时
+     */
+    public static final int HOUR   = 60 * MINUTE;
+    /**
+     * 一天
+     */
+    public static final int DAY    = 24 * HOUR;
 }
