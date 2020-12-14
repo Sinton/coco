c80a3e91806f6790e96bdb62b1abaf47c1e4942a
Sinton <yansuntong@gmail.com>
2020-12-14 22:25:41 +0800

    database initialzation for application startup

---
+++ src/main/resources/sqls/0.0.1/schema.sql
@@ -0,0 +1,76 @@
+create table if not exists t_endpoint
+(
+    id               integer not null constraint t_endpoints_pk primary key autoincrement,
+    name             text    not null,
+    port             integer default 2375 not null,
+    public_ip        text,
+    endpoint_url     text,
+    tls_enable       integer default 0,
+    resources        text,
+    docker_config    text,
+    status           integer default 0,
+    update_date_time integer,
+    tls_config       text,
+    endpoint_type    integer default 1,
+    owner            int
+);
+
+create unique index if not exists t_endpoint_id_uindex on t_endpoint (id);
+
+create table if not exists t_host
+(
+    id         integer not null constraint t_host_pk primary key autoincrement,
+    ip         text    not null,
+    port       integer default 22,
+    user       text    default 'root',
+    password   text,
+    resource   text,
+    hostname   text,
+    dockerized char(1) default 0
+);
+
+create unique index if not exists t_host_id_uindex on t_host (id);
+
+create table if not exists t_notification
+(
+    id          integer not null constraint t_notification_pk primary key autoincrement,
+    content     text,
+    status      integer default 0 not null,
+    create_date integer
+);
+
+create table if not exists t_setting
+(
+    id    text not null constraint t_setting_pk primary key,
+    key   text,
+    value text,
+    owner int
+);
+
+create table if not exists t_stack
+(
+    id           text not null constraint t_stack_pk primary key,
+    name         text default '' not null,
+    status       integer default '0' not null,
+    type         integer default 0,
+    endpoint     text,
+    swarm_id     text,
+    project_path text
+);
+
+create table if not exists t_user
+(
+    uid             integer not null constraint t_user_pk primary key autoincrement,
+    username        text    not null,
+    salt            text    not null,
+    password        text    not null,
+    nickname        text,
+    avatar          text    default 'avatar.jpg',
+    status          integer default 0,
+    email           text,
+    telephone       text,
+    lang            text    default 'zh-CN',
+    last_login_ip   text,
+    last_login_time integer,
+    create_time     integer
+);
