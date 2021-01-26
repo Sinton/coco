12d26d8be4b43442731abbd26886783d02ea6e46
Sinton <yansuntong@gmail.com>
2021-01-27 01:39:34 +0800

    improve swarm config create logic

--- src/main/java/com/github/coco/controller/ConfigController.java
+++ src/main/java/com/github/coco/controller/ConfigController.java
@@ -3,11 +3,13 @@ package com.github.coco.controller;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import com.github.coco.annotation.WebLog;
+import com.github.coco.constant.DbConstant;
 import com.github.coco.constant.GlobalConstant;
 import com.github.coco.constant.dict.ErrorCodeEnum;
-import com.github.coco.utils.LoggerHelper;
+import com.spotify.docker.client.exceptions.DockerException;
 import com.spotify.docker.client.messages.swarm.Config;
 import com.spotify.docker.client.messages.swarm.ConfigSpec;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +24,7 @@ import java.util.Objects;
 /**
  * @author Yan
  */
+@Slf4j
 @RestController
 @RequestMapping(value = "/api/config")
 public class ConfigController extends BaseController {
@@ -29,20 +32,22 @@ public class ConfigController extends BaseController {
     @WebLog
     @PostMapping(value = "/create")
     public Map<String, Object> createConfig(@RequestBody Map<String, Object> params) {
-        String name = params.getOrDefault("name", null).toString();
-        String data = params.getOrDefault("data", null).toString();
-        Map<String, String> labels = JSON.parseObject(Objects.toString(params.get("labels"), ""),
+        String name = Objects.toString(params.get("name"), "");
+        String data = Objects.toString(params.get("data"), "");
+        Map<String, String> labels = JSON.parseObject(JSON.toJSONString(params.get("labels")),
                                                       new TypeReference<Map<String, String>>() {});
         try {
-            ConfigSpec configSpec = ConfigSpec.builder()
-                                              .name(name)
-                                              .data(Base64.getEncoder().encodeToString(data.getBytes(StandardCharsets.UTF_8)))
-                                              .labels(labels.isEmpty() ? null : labels)
-                                              .build();
-            return apiResponseDTO.returnResult(GlobalConstant.SUCCESS_CODE,
-                                               getDockerClient().createConfig(configSpec));
-        } catch (Exception e) {
-            LoggerHelper.fmtError(getClass(), e, "创建容器配置项失败");
+            String encodedData = Base64.getEncoder().encodeToString(data.getBytes(StandardCharsets.UTF_8));
+            ConfigSpec.Builder configSpecBuilder = ConfigSpec.builder()
+                                                             .name(name)
+                                                             .data(encodedData);
+            if (!labels.isEmpty()) {
+                configSpecBuilder.labels(labels);
+            }
+            getDockerClient().createConfig(configSpecBuilder.build());
+            return apiResponseDTO.returnResult(GlobalConstant.SUCCESS_CODE, "创建Swarm配置项成功");
+        } catch (DockerException | InterruptedException e) {
+            log.error("创建Swarm配置项失败", e);
             return apiResponseDTO.returnResult(ErrorCodeEnum.EXCEPTION.getCode(), e);
         }
     }
@@ -59,10 +64,10 @@ public class ConfigController extends BaseController {
                                                    .data(configSpec.data())
                                                    .labels(configSpec.labels())
                                                    .build();
-            return apiResponseDTO.returnResult(GlobalConstant.SUCCESS_CODE,
-                                               getDockerClient().createConfig(cloneConfigSpec));
-        } catch (Exception e) {
-            LoggerHelper.fmtError(getClass(), e, "克隆容器配置项失败");
+            getDockerClient().createConfig(cloneConfigSpec);
+            return apiResponseDTO.returnResult(GlobalConstant.SUCCESS_CODE, "克隆Swarm配置项成功");
+        } catch (DockerException | InterruptedException e) {
+            log.error("克隆Swarm配置项失败", e);
             return apiResponseDTO.returnResult(ErrorCodeEnum.EXCEPTION.getCode(), e);
         }
     }
@@ -73,9 +78,9 @@ public class ConfigController extends BaseController {
         String configId = Objects.toString(params.get("configId"), "");
         try {
             getDockerClient().deleteConfig(configId);
-            return apiResponseDTO.returnResult(GlobalConstant.SUCCESS_CODE, "删除配置项成功");
-        } catch (Exception e) {
-            LoggerHelper.fmtError(getClass(), e, "删除容器配置项失败");
+            return apiResponseDTO.returnResult(GlobalConstant.SUCCESS_CODE, "删除Swarm配置项成功");
+        } catch (DockerException | InterruptedException e) {
+            log.error("删除Swarm配置项失败", e);
             return apiResponseDTO.returnResult(ErrorCodeEnum.EXCEPTION.getCode(), e);
         }
     }
@@ -85,7 +90,7 @@ public class ConfigController extends BaseController {
     public Map<String, Object> updateConfig(@RequestBody Map<String, Object> params) {
         String configId            = Objects.toString(params.get("configId"), "");
         String data                = Objects.toString(params.get("data"), "");
-        Map<String, String> labels = JSON.parseObject(Objects.toString(params.get("data"), ""),
+        Map<String, String> labels = JSON.parseObject(Objects.toString(params.get("labels"), "{}"),
                                                       new TypeReference<Map<String, String>>() {});
         try {
             Config config = getDockerClient().inspectConfig(configId);
@@ -94,9 +99,9 @@ public class ConfigController extends BaseController {
                                               .labels(labels)
                                               .build();
             getDockerClient().updateConfig(config.id(), config.version().index(), configSpec);
-            return apiResponseDTO.returnResult(GlobalConstant.SUCCESS_CODE, "修改配置项成功");
-        } catch (Exception e) {
-            LoggerHelper.fmtError(getClass(), e, "修改容器配置项失败");
+            return apiResponseDTO.returnResult(GlobalConstant.SUCCESS_CODE, "修改Swarm配置项成功");
+        } catch (DockerException | InterruptedException e) {
+            log.error("修改Swarm配置项失败", e);
             return apiResponseDTO.returnResult(ErrorCodeEnum.EXCEPTION.getCode(), e);
         }
     }
@@ -104,14 +109,14 @@ public class ConfigController extends BaseController {
     @WebLog
     @PostMapping(value = "/list")
     public Map<String, Object> getPageConfigs(@RequestBody Map<String, Object> params) {
-        int pageNo = Integer.parseInt(params.getOrDefault("pageNo", 1).toString());
-        int pageSize = Integer.parseInt(params.getOrDefault("pageSize", 10).toString());
+        int pageNo = Integer.parseInt(String.valueOf(params.getOrDefault("pageNo", DbConstant.PAGE_NO)));
+        int pageSize = Integer.parseInt(String.valueOf(params.getOrDefault("pageSize", DbConstant.PAGE_SIZE)));
         try {
             List<Config> configs = getDockerClient().listConfigs();
             return apiResponseDTO.returnResult(GlobalConstant.SUCCESS_CODE,
                                                apiResponseDTO.tableResult(pageNo, pageSize, configs));
-        } catch (Exception e) {
-            LoggerHelper.fmtError(getClass(), e, "获取容器配置项列表失败");
+        } catch (DockerException | InterruptedException e) {
+            log.error("获取Swarm配置项列表失败", e);
             return apiResponseDTO.returnResult(ErrorCodeEnum.EXCEPTION.getCode(), e);
         }
     }
@@ -123,8 +128,8 @@ public class ConfigController extends BaseController {
         try {
             return apiResponseDTO.returnResult(GlobalConstant.SUCCESS_CODE,
                                                getDockerClient().inspectConfig(configId));
-        } catch (Exception e) {
-            LoggerHelper.fmtError(getClass(), e, "获取容器配置项摘要信息失败");
+        } catch (DockerException | InterruptedException e) {
+            log.error("获取Swarm配置项摘要信息失败", e);
             return apiResponseDTO.returnResult(ErrorCodeEnum.EXCEPTION.getCode(), e);
         }
     }
