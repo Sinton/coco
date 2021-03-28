14b2dac3e8f0fd35d59dae093999dfea2fb463c9
Sinton <yansuntong@gmail.com>
2021-03-28 10:12:00 +0800

    fixed bug

--- src/main/java/com/github/coco/constant/ErrorConstant.java
+++ src/main/java/com/github/coco/constant/ErrorConstant.java
@@ -9,12 +9,23 @@ import java.util.Map;
 public class ErrorConstant {
     public static Map<Integer, String> ErrorMapping = new HashMap<>(16);
     /**
-     * API接口调用发生错误
+     * API接口调用发生错误异常
      */
-    public static final int ERR_API_INVOKE = 4000;
-    public static final int ERR_BASE_COMMON = 4000;
+    public static final int ERR_API_INVOKE         = 4000;
+
+    /**
+     * 业务执行出现未知异常错误
+     */
+    public static final int ERR_BASE_COMMON        = 4000;
+
+    /**
+     * Token失效错误异常
+     */
+    public static final int ERR_AUTH_TOEKN_EXPIRED = 9026;
 
     static {
-        ErrorMapping.put(ERR_BASE_COMMON, "业务执行出现未知异常");
+        ErrorMapping.put(ERR_API_INVOKE, "API接口调用发生错误异常");
+        ErrorMapping.put(ERR_BASE_COMMON, "业务执行出现未知错误异常");
+        ErrorMapping.put(ERR_AUTH_TOEKN_EXPIRED, "Token失效错误异常");
     }
 }
