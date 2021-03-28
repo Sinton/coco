5b5616df5b26e3fd786388e1489814b31659d7bb
Sinton <yansuntong@gmail.com>
2021-03-28 10:11:15 +0800

    optimizing code

--- src/main/java/com/github/coco/controller/SwarmController.java
+++ src/main/java/com/github/coco/controller/SwarmController.java
@@ -3,8 +3,10 @@ package com.github.coco.controller;
 import com.github.coco.annotation.WebLog;
 import com.github.coco.constant.GlobalConstant;
 import com.github.coco.constant.dict.ErrorCodeEnum;
-import com.github.coco.utils.LoggerHelper;
+import com.spotify.docker.client.exceptions.DockerException;
 import com.spotify.docker.client.messages.swarm.SwarmInit;
+import com.spotify.docker.client.messages.swarm.SwarmJoin;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,7 @@ import java.util.Objects;
 /**
  * @author Yan
  */
+@Slf4j
 @RestController
 @RequestMapping(value = "/api/swarm")
 public class SwarmController extends BaseController {
@@ -23,9 +26,9 @@ public class SwarmController extends BaseController {
     @WebLog
     @PostMapping(value = "/init")
     public Map<String, Object> initSwarm(@RequestBody Map<String, Object> params) {
-        String ip = Objects.toString(params.get("ip"), "192.168.3.140");
+        String ip = Objects.toString(params.get("ip"), "localhost");
         try {
-            if (getDockerClient().info().swarm() == null) {
+            if (getDockerClient().info().swarm() == null || !getDockerClient().info().swarm().controlAvailable()) {
                 SwarmInit swarmInit = SwarmInit.builder()
                                                .advertiseAddr(ip)
                                                .listenAddr(ip)
@@ -36,8 +39,24 @@ public class SwarmController extends BaseController {
             }
             return apiResponseDTO.returnResult(ErrorCodeEnum.EXCEPTION.getCode(), "已Swarm集群化");
         } catch (Exception e) {
-            LoggerHelper.fmtError(getClass(), e, "初始化Swarm集群节点失败");
+            log.error("初始化Swarm集群节点失败", e);
             return apiResponseDTO.returnResult(ErrorCodeEnum.EXCEPTION.getCode(), "初始化Swarm集群节点失败");
+        }
+    }
+
+    @WebLog
+    @PostMapping(value = "/join")
+    public Map<String, Object> joinSwarm(@RequestBody Map<String, Object> params) {
+        try {
+            String joinToken = getDockerClient().inspectSwarm().joinTokens().worker();
+            SwarmJoin swarmJoin = SwarmJoin.builder()
+                                           .joinToken(joinToken)
+                                           .build();
+            getDockerClient().joinSwarm(swarmJoin);
+            return apiResponseDTO.returnResult(GlobalConstant.SUCCESS_CODE, "");
+        } catch (DockerException | InterruptedException e) {
+            log.error("集群节点创建失败", e);
+            return apiResponseDTO.returnResult(ErrorCodeEnum.EXCEPTION.getCode(), "集群节点创建失败");
         }
     }
 
@@ -47,7 +66,7 @@ public class SwarmController extends BaseController {
         try {
             return apiResponseDTO.returnResult(GlobalConstant.SUCCESS_CODE, getDockerClient().inspectSwarm());
         } catch (Exception e) {
-            LoggerHelper.fmtError(getClass(), e, "获取集群节点信息失败");
+            log.error("获取集群节点信息失败", e);
             return apiResponseDTO.returnResult(ErrorCodeEnum.EXCEPTION.getCode(), "获取集群节点信息失败");
         }
     }
