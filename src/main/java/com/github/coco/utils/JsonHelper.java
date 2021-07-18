ea21bbba96445109edea6174d4db9a5698d2a79b
Sinton <yansuntong@gmail.com>
2021-07-18 16:49:50 +0800

    migrate StringHelper to JsonHelper

---
+++ src/main/java/com/github/coco/utils/JsonHelper.java
@@ -0,0 +1,33 @@
+package com.github.coco.utils;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+
+import java.util.Map;
+
+/**
+ * @author Yan
+ */
+public class JsonHelper {
+    /**
+     * JSON字符串转Map集合
+     *
+     * @param resource
+     * @return
+     */
+    public static <K, V> Map<K, V> jsonStringConvertMap(String resource) {
+        return JSON.parseObject(resource, new TypeReference<Map<K, V>>() {});
+    }
+
+    /**
+     * JSON字符串转Java对象
+     *
+     * @param resource
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> T jsonStringConvertObject(String resource, Class<T> clazz) {
+        return JSON.parseObject(resource, clazz);
+    }
+}
